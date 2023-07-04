758b8e6c60bbbafef1dc878c5128c61729c898ad
Francisco Gonzalez <gzmorell@gmail.com>
2023-07-04 02:52:06 +0200

    Trying to config rustfmt

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 return {
     -- Configure AstroNvim updates
     updater = {
-        remote = "origin", -- remote to use
-        channel = "stable", -- "stable" or "nightly"
-        version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-        branch = "nightly", -- branch name (NIGHTLY ONLY)
-        commit = nil, -- commit hash (NIGHTLY ONLY)
-        pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-        skip_prompts = false, -- skip prompts about breaking changes
+        remote = "origin",     -- remote to use
+        channel = "stable",    -- "stable" or "nightly"
+        version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+        branch = "nightly",    -- branch name (NIGHTLY ONLY)
+        commit = nil,          -- commit hash (NIGHTLY ONLY)
+        pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+        skip_prompts = false,  -- skip prompts about breaking changes
         show_changelog = true, -- show the changelog after performing an update
-        auto_quit = false, -- automatically quit the current session after a successful update
-        remotes = { -- easily add new remotes to track
+        auto_quit = false,     -- automatically quit the current session after a successful update
+        remotes = {            -- easily add new remotes to track
             --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
             --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
             --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -21,15 +21,16 @@ return {
     colorscheme = "astrodark",
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {virtual_text = true, underline = true},
+    diagnostics = { virtual_text = true, underline = true },
 
     lsp = {
         -- customize lsp formatting options
         formatting = {
             -- control auto formatting on save
             format_on_save = {
-                enabled = true, -- enable or disable format on save globally
+                enabled = true,     -- enable or disable format on save globally
                 allow_filetypes = { -- enable format on save for specified filetypes only
+                    "cpp", "rust",
                     -- "go",
                 },
                 ignore_filetypes = { -- disable format on save for specified filetypes
@@ -52,33 +53,32 @@ return {
 
         setup_handlers = {
             -- add custom handler
-            clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
-            rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end
+            clangd = function(_, opts)
+                require("clangd_extensions").setup { server = opts }
+            end,
+            rust_analyzer = function(_, opts)
+                require("rust-tools").setup { server = opts }
+            end
         },
-        config = {
-            clangd = {capabilities = {offsetEncoding = "utf-8"}}
-        },
+        config = { clangd = { capabilities = { offsetEncoding = "utf-8" } } }
     },
     plugins = {
         "p00f/clangd_extensions.nvim", -- install lsp plugin
         {
-          "williamboman/mason-lspconfig.nvim",
-          opts = {
-            ensure_installed = { "clangd" }, -- automatically install lsp
-          },
-        },
-        "simrat39/rust-tools.nvim", -- add lsp plugin
-        {
             "williamboman/mason-lspconfig.nvim",
             opts = {
-                ensure_installed = { "rust_analyzer" },
-            },
-        },
+                ensure_installed = { "clangd" } -- automatically install lsp
+            }
+        }, "simrat39/rust-tools.nvim",        -- add lsp plugin
+        {
+            "williamboman/mason-lspconfig.nvim",
+            opts = { ensure_installed = { "rust_analyzer" } }
+        }
     },
 
     -- Configure require("lazy").setup() options
     lazy = {
-        defaults = {lazy = true},
+        defaults = { lazy = true },
         performance = {
             rtp = {
                 -- customize default disabled vim plugins
